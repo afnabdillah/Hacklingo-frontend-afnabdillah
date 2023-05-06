060e39b04638bff685a5553586a706e456b1ad91
perpuu <permanaputra543@gmail.com>
2023-05-06 16:40:43 +0700

    feat: landingpage

---
+++ sandbox/screens/Chat.js
@@ -0,0 +1,154 @@
+import React, {
+  useState,
+  useEffect,
+  useLayoutEffect,
+  useCallback,
+  useContext
+} from 'react';
+import { TouchableOpacity, Text } from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat';
+import {
+  collection,
+  addDoc,
+  orderBy,
+  query,
+  onSnapshot,
+  where,
+  doc,
+  getDocs
+} from 'firebase/firestore';
+import { signOut } from 'firebase/auth';
+import AuthenticatedUserContext from '../helper/AuthenticatedUserContext'
+import { auth, database } from '../config/firebase';
+
+export default function Chat({ route, navigation }) {
+  const [messages, setMessages] = useState([]);
+  const { recipientEmail, recipientName, senderEmail } = route.params;
+  // console.log(route.params, "<<< recipientUsername")
+  console.log(recipientEmail, recipientName, senderEmail)
+  // console.log(route.params)
+  const { user: currentUser } = useContext(AuthenticatedUserContext);
+  const [currentUserData, setCurrentUserData] = useState(null);
+
+  async function getUserDataByEmail(email) {
+    const usersCollectionRef = collection(database, 'users');
+    const q = query(usersCollectionRef, where('email', '==', email));
+    const querySnapshot = await getDocs(q);
+
+    if (!querySnapshot.empty) {
+      const userDoc = querySnapshot.docs[0];
+      return userDoc.data();
+    } else {
+      return null;
+    }
+  }
+
+  useEffect(() => {
+    async function fetchCurrentUserData() {
+      const userData = await getUserDataByEmail(currentUser.email);
+      setCurrentUserData(userData);
+    }
+
+    fetchCurrentUserData();
+  }, [currentUser]);
+  const mergeMessages = (oldMessages, newMessages) => {
+    const allMessages = [...oldMessages, ...newMessages];
+    const uniqueMessages = allMessages.filter(
+      (message, index, self) =>
+        index === self.findIndex((m) => m._id === message._id)
+    );
+
+    return uniqueMessages.sort((a, b) => b.createdAt - a.createdAt);
+  };
+  const onSignOut = () => {
+    signOut(auth).catch(error => console.log('Error logging out: ', error));
+  };
+
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity
+          style={{
+            marginRight: 10
+          }}
+          onPress={onSignOut}
+        >
+          <Text>Logout</Text>
+        </TouchableOpacity>
+      )
+    });
+  }, [navigation]);
+
+  useEffect(() => {
+    const collectionRef = collection(database, 'chats');
+    const currentUserEmail = auth.currentUser.email;
+  
+    const q = query(
+      collectionRef,
+      orderBy('createdAt', 'desc')
+    );
+  
+    const unsubscribe = onSnapshot(q, querySnapshot => {
+      const fetchedMessages = querySnapshot.docs
+        .map(doc => ({
+          _id: doc.data()._id,
+          createdAt: doc.data().createdAt.toDate(),
+          text: doc.data().text,
+          user: doc.data().user,
+          recipient: doc.data().recipient,
+        }))
+        .filter(message => {
+          // console.log(message, "<<< message")
+          return (
+            (message.user._id === currentUserEmail &&
+              message.recipient === recipientEmail) ||
+            (message.user._id === recipientEmail &&
+              message.recipient === currentUserEmail)
+          );
+        });
+      setMessages((messages) => mergeMessages(messages, fetchedMessages));
+    });
+  
+    return () => {
+      unsubscribe();
+    };
+  }, [recipientEmail]);
+
+
+  const onSend = useCallback((messages = []) => {
+    if (!currentUserData) {
+      console.error('User data not loaded yet. Please try again later.');
+      return;
+    }
+
+    setMessages(previousMessages => GiftedChat.append(previousMessages, messages));
+    const { _id, createdAt, text, user } = messages[0];
+    addDoc(collection(database, 'chats'), {
+      _id,
+      createdAt,
+      text,
+      user: {
+        _id: currentUser.email,
+        username: currentUserData.username,
+        avatar: currentUserData.avatar || 'https://i.pravatar.cc/300',
+      },
+      recipient: recipientEmail,
+      recipientName: recipientName,
+    });
+  }, [currentUserData]);
+
+
+  return (
+    <GiftedChat
+      messages={messages}
+      showAvatarForEveryMessage={true}
+      onSend={messages => onSend(messages)}
+      user={{
+        _id: currentUser.email,
+        username: currentUser.username,
+        avatar: currentUser.avatar || 'https://i.pravatar.cc/300'
+      }}
+    />
+  );
+}
