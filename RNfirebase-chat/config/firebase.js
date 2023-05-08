e869c1a8314fd3d8b2391e2fab77348565deec31
perpuu <permanaputra543@gmail.com>
2023-05-08 17:10:37 +0700

    feat : nambah package reanimated

--- RNfirebase-chat/config/firebase.js
+++ RNfirebase-chat/config/firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import { initializeFirestore } from 'firebase/firestore';
 import Constants from 'expo-constants';
 
 // Firebase config
@@ -15,7 +15,10 @@ const firebaseConfig = {
 };
 
 // initialize firebase
-initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth();
-export const database = getFirestore();
+export const database = initializeFirestore(app, {
+  experimentalForceLongPolling: true, // this line
+  useFetchStreams: false, // and this line
+});
