93c5b33a23b8215a445c9057c09d72b6dd50adfc
sulthanna97 <sulthannaufal1997@gmail.com>
2023-05-09 13:12:23 +0700

    fix merge with dev-feature-chat

--- RNfirebase-chat/screens/Landingpage/Carousel.js
+++ RNfirebase-chat/screens/Landingpage/Carousel.js
@@ -9,85 +9,12 @@ function infiniteScroll(dataList, flatListRef) {
   let scrollValue = 0;
   let scrolled = 0;
 
-<<<<<<< HEAD
   setInterval(() => {
     scrolled++;
     if (scrolled < numberOfData) scrollValue = scrollValue + width;
     else {
       scrollValue = 0;
       scrolled = 0;
-=======
-function infiniteScroll(dataList){
-    const numberOfData = dataList.length
-    let scrollValue = 0, scrolled = 0
-
-    setInterval(function() {
-        scrolled ++
-        if(scrolled < numberOfData)
-        scrollValue = scrollValue + width
-
-        else{
-            scrollValue = 0
-            scrolled = 0
-        }
-
-        this.flatList?.scrollToOffset({ animated: true, offset: scrollValue})
-        
-    }, 3000)
-}
-
-
-const Carousel = ({ data }) => {
-    const scrollX = new Animated.Value(0)
-    let position = Animated.divide(scrollX, width)
-    const [dataList, setDataList] = useState(data)
-
-    useEffect(()=> {
-        setDataList(data)
-        infiniteScroll(dataList)
-    })
-
-
-    if (data && data.length) {
-        return (
-            <View>
-                <FlatList data={data}
-                ref = {(flatList) => {this.flatList = flatList}}
-                    keyExtractor={(item, index) => 'key' + index}
-                    horizontal
-                    pagingEnabled
-                    scrollEnabled
-                    snapToAlignment="center"
-                    scrollEventThrottle={16}
-                    decelerationRate={"fast"}
-                    showsHorizontalScrollIndicator={false}
-                    renderItem={({ item }) => {
-                        return <CarouselItem item={item} />
-                    }}
-                    onScroll={Animated.event(
-                        [{ nativeEvent: { contentOffset: { x: scrollX } } }]
-                    )}
-                />
-
-                <View style={styles.dotView}>
-                    {data.map((_, i) => {
-                        let opacity = position.interpolate({
-                            inputRange: [i - 1, i, i + 1],
-                            outputRange: [0.3, 1, 0.3],
-                            extrapolate: 'clamp'
-                        })
-                        return (
-                            <Animated.View
-                                key={i}
-                                style={{ opacity, height: 10, width: 10, backgroundColor: '#ff8c00', margin: 8, borderRadius: 5 }}
-                            />
-                        )
-                    })}
-
-                </View>
-            </View>
-        )
->>>>>>> 7855a450401c43b9681136964c1f7f8561baef93
     }
 
     if (flatListRef.current) {
